1c78ede1d1da1de4e642f4ff18a8813bace54554
Emperor <emperorjacoba@gmail.com>
2026-03-28 16:15:44 -0400

    Add functionality to metadata input boxes

--- PenguinChartEditor/Assets/Project/Scripts/UI/SongSetupUI/MetadataInput.cs
+++ PenguinChartEditor/Assets/Project/Scripts/UI/SongSetupUI/MetadataInput.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -5,4 +6,19 @@ public class MetadataInput : MonoBehaviour
 {
     [SerializeField] TMP_InputField inputField;
     [SerializeField] private Metadata.MetadataType targetMetadata;
+
+    private void Awake()
+    {
+        if (Chart.Metadata.SongInfo.TryGetValue(targetMetadata, out var value))
+        {
+            inputField.text = value;
+        }
+
+        inputField.onValueChanged.AddListener(UpdateMetadata);
+    }
+
+    private void UpdateMetadata(string input)
+    {
+        Chart.Metadata.SongInfo[targetMetadata] = input;
+    }
 }
