fe87a9b351a2cef40509e577daaf30c71a59bc2b
Emperor <emperorjacoba@gmail.com>
2026-03-28 16:07:00 -0400

    Create new song setup scene

---
+++ PenguinChartEditor/Assets/Project/Scripts/UI/SongSetupUI/MetadataInput.cs
@@ -0,0 +1,8 @@
+using TMPro;
+using UnityEngine;
+
+public class MetadataInput : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] private Metadata.MetadataType targetMetadata;
+}
