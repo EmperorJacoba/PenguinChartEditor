42f7d54c8a0699ef03d284341169ab5e35a6fed2
Emperor <emperorjacoba@gmail.com>
2026-03-28 22:03:31 -0400

    Make setup tab switcher

---
+++ PenguinChartEditor/Assets/Project/Scripts/UI/PenguinTabImages.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class PenguinTabImages : ScriptableObject
+{
+    [SerializeField] public Sprite tabActiveImage;
+    [SerializeField] public Sprite tabInactiveImage;
+}
---
+++ PenguinChartEditor/Assets/Project/Scripts/UI/SongSetupUI/SetupTabSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SetupTabSwitcher : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private PenguinTabImages imageReference;
+    private Image imageComponent;
+    public GameObject controlledComponent;
+
+    private static readonly List<SetupTabSwitcher> tabs = new();
+
+    private void Awake()
+    {
+        imageComponent = GetComponent<Image>();
+        tabs.Add(this);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (imageComponent.sprite == imageReference.tabActiveImage) return;
+        
+        controlledComponent.SetActive(true);
+        foreach (var tab in tabs.Where(x => x != this))
+        {
+            tab.SwitchOff();
+        }
+
+        imageComponent.sprite = imageReference.tabActiveImage;
+    }
+
+    private void SwitchOff()
+    {
+        controlledComponent.SetActive(false);
+        imageComponent.sprite = imageReference.tabInactiveImage;
+    }
+}
