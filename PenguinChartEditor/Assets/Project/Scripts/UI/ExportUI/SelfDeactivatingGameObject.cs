e3f7bc061614c1a1c3eea4a4faf52b6baf9494bc
Emperor <emperorjacoba@gmail.com>
2026-04-09 19:12:33 -0400

    Fix null ref exceptions upon export without accessing other export tabs

---
+++ PenguinChartEditor/Assets/Project/Scripts/UI/ExportUI/SelfDeactivatingGameObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class SelfDeactivatingGameObject : MonoBehaviour
+{
+    // Unity refuses to run the Start() functions properly if you try to quickly enable them and disable them upon a scene start.
+    // Start() needs to run in a lot of objects in the ExportScene to avoid a bunch of NullReferenceExceptions in the case
+    // that the user does not access certain tabs. So, the hidden tab content has to be active pre-start and then 
+    // quickly disabled once playing. 
+    private void LateUpdate()
+    {
+        // This will happen after all the Start() functions are called, because it's *Late*Update(). Once it's disabled,
+        // it's not running again. 
+        gameObject.SetActive(false);
+    }
+}
