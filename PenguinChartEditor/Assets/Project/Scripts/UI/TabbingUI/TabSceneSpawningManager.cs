b524fc5b2034a7b5b6c310cc8e9a047ec953c42f
Emperor <emperorjacoba@gmail.com>
2026-04-14 15:47:05 -0400

    Rework FiveFretChartingScene UI to work with tab ribbon

--- PenguinChartEditor/Assets/Project/Scripts/UI/TabbingUI/TabSceneSpawningManager.cs
+++ PenguinChartEditor/Assets/Project/Scripts/UI/TabbingUI/TabSceneSpawningManager.cs
@@ -4,15 +4,45 @@ using UnityEngine.SceneManagement;
 
 public class TabSceneSpawningManager : MonoBehaviour
 {
-    // When tabbing scene is active, the canvas resizer must be overriden to conform to the ribbon. When testing/debugging,
-    // it is much easier to run scenes standalone. 
-    public static bool containerSceneIsActive { get; private set; }
-    private static TabSceneSpawningManager instance;
+    private bool tabbingRibbonActive = false;
+    // Should be the ribbon border. 105 pixels tall as of writing this
+    [SerializeField] private RectTransform widestRibbonElement;
+    public static TabSceneSpawningManager instance;
+
+    public static Vector2 RealUISize
+    {
+        get
+        {
+            return instance is not null && instance.tabbingRibbonActive
+                ? new Vector2(Screen.width, Screen.height - instance.widestRibbonElement.rect.height)
+                : new Vector2(Screen.width, Screen.height);
+        }
+    }
+    public static Vector2 RealUICenter
+    {
+        get
+        {
+            return instance is not null && instance.tabbingRibbonActive
+                ? new Vector2(Screen.width / 2.0f, (Screen.height - instance.widestRibbonElement.rect.height) / 2.0f)
+                : new Vector2(Screen.width / 2.0f, Screen.height / 2.0f);
+        }
+    }
+
+    public static float YCenterDelta
+    {
+        get
+        {
+            return (Screen.height / 2.0f) - (RealUICenter.y);
+        }
+    }
+
+    // Use this to adjust the Y position of canvas elements to be centered properly in their window.
+    public float HeightOffset => widestRibbonElement.rect.height;
     
     private void Awake()
     {
+        tabbingRibbonActive = true;
         instance = this;
-        containerSceneIsActive = true;
     }
 
     private void Start()
---
+++ PenguinChartEditor/Assets/Project/Scripts/UI/TabbingUI/UIElementAdjustor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class UIElementAdjustor : MonoBehaviour
+{
+    private Vector2 targetCoordinates;
+    private RectTransform rt;
+    private void OnEnable()
+    {
+        rt = GetComponent<RectTransform>();
+        targetCoordinates = rt.anchoredPosition;
+        
+        PenguinSceneTabScaler.ScreenSizeUpdated += MoveElement;
+    }
+
+    private void OnDisable()
+    {
+        PenguinSceneTabScaler.ScreenSizeUpdated -= MoveElement;
+    }
+
+    private void MoveElement(float newCenterDelta)
+    {
+        rt.anchoredPosition = new Vector2(targetCoordinates.x, targetCoordinates.y - newCenterDelta);
+    }
+}
