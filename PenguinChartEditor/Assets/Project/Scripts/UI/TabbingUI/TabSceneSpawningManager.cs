9c359e074269269d8a7f3f7395bc8c5138c730fa
Emperor <emperorjacoba@gmail.com>
2026-04-14 13:00:39 -0400

    Save FFCS changes to canvas assets

---
+++ PenguinChartEditor/Assets/Project/Scripts/UI/TabbingUI/TabSceneSpawningManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TabSceneSpawningManager : MonoBehaviour
+{
+    // When tabbing scene is active, the canvas resizer must be overriden to conform to the ribbon. When testing/debugging,
+    // it is much easier to run scenes standalone. 
+    public static bool containerSceneIsActive { get; private set; }
+    private static TabSceneSpawningManager instance;
+    
+    private void Awake()
+    {
+        instance = this;
+        containerSceneIsActive = true;
+    }
+
+    private void Start()
+    {
+        SceneManager.LoadScene("FiveFretChartingScene", LoadSceneMode.Additive);
+    }
+}
