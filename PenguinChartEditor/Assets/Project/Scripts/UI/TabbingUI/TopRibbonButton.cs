d88cb033c513b2de4421af67908a32cf7969ead7
Emperor <emperorjacoba@gmail.com>
2026-04-19 18:18:56 -0400

    Set up top ribbon menus

---
+++ PenguinChartEditor/Assets/Project/Scripts/UI/TabbingUI/TopRibbonButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class TopRibbonButton : MonoBehaviour
+{
+    [SerializeField] private GameObject controlledPanel;
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(EnableContent);
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame && controlledPanel.activeInHierarchy)
+        {
+            controlledPanel.SetActive(false);
+        }
+    }
+
+    private void EnableContent()
+    {
+        controlledPanel.SetActive(true);
+    }
+}
