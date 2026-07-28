30c2cab5a6533310d7982c94553f21356e6fbe2b
Emperor <emperorjacoba@gmail.com>
2026-07-27 20:50:42 -0400

    Make loading dialog prefab + add to dialog manager

--- PenguinChartEditor/Assets/Project/Scripts/Dialogs/DialogManager.cs
+++ PenguinChartEditor/Assets/Project/Scripts/Dialogs/DialogManager.cs
@@ -11,6 +11,7 @@ namespace Penguin.Dialogs
         [SerializeField] private ConfirmationDialog confirmationDialog;
         [SerializeField] private DataWipeDialog dataWipeDialog;
         [SerializeField] private ErrorNotificationDialog errorNotificationDialog;
+        [SerializeField] private LoadingDialog loadingDialog;
         
         public static T SpawnDialog<T>()
         {
@@ -19,6 +20,7 @@ namespace Penguin.Dialogs
             if (typeof(T) == typeof(ErrorNotificationDialog)) return (T)(object)instance.errorNotificationDialog;
             if (typeof(T) == typeof(ConfirmationDialog)) return (T)(object)instance.confirmationDialog;
             if (typeof(T) == typeof(DataWipeDialog)) return (T)(object)instance.dataWipeDialog;
+            if (typeof(T) == typeof(LoadingDialog)) return (T)(object)(instance.loadingDialog);
             
             throw new ArgumentOutOfRangeException($"No support for creating dialog of type {typeof(T)}");
         }
---
+++ PenguinChartEditor/Assets/Project/Scripts/Dialogs/LoadingDialog.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Penguin.Dialogs
+{
+    public class LoadingDialog : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text titleText;
+        [SerializeField] private TMP_Text descriptionText;
+        
+        public void Initialize(string title, float width = 1660, float height = 860)
+        {
+            gameObject.SetActive(true);
+            
+            titleText.text = title;
+            descriptionText.text = "Operation started.";
+            var rt = GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(width, height);
+            rt.anchoredPosition = Vector3.zero;
+        }
+
+        public void UpdateLoadingState(string flavorText)
+        {
+            descriptionText.text = flavorText;
+        }
+    }
+}
