a9504b8925257851cdea7829206a9d088134d0ed
Emperor (Jacob Russell) <emperorjacoba@gmail.com>
2026-09-06 13:55:25 -0400

    Change editor plugin file paths for linux

--- PenguinChartEditor/Assets/Project/Scripts/Audio/AudioManager.cs
+++ PenguinChartEditor/Assets/Project/Scripts/Audio/AudioManager.cs
@@ -147,12 +147,11 @@ public class AudioManager : MonoBehaviour
             return;
         }
 
-        string pluginPath = $"{Application.dataPath}/Plugins/";
+        string pluginPath = $"{Application.dataPath}/Plugins/Bass/";
 
 #if UNITY_EDITOR_WIN && UNITY_EDITOR
-        pluginPath += "Bass/Bass_win";
+        pluginPath += "Bass_win";
 #endif
-
 #if UNITY_STANDALONE_WIN && !UNITY_EDITOR
         pluginPath += "x86_64";
 #endif
@@ -160,15 +159,26 @@ public class AudioManager : MonoBehaviour
 #if (UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX)
         pluginPath += "Bass_macOS";
 #endif
-#if (UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX)
-        pluginPath += "Bass_linux/x86_64";
+        
+#if UNITY_EDITOR_LINUX && UNITY_EDITOR
+        pluginPath += "Bass_linux/x86_64/";
 #endif
-
+        
+#if UNITY_STANDALONE_LINUX && !UNITY_EDITOR
+        pluginPath += "FIXME";
+#endif
+        
         foreach (var file in Directory.EnumerateFiles(pluginPath))
         {
             if (file.Contains("meta")) continue;
             var fileName = Path.GetFileName(file);
-            if (fileName == "bass.dll" || fileName.Contains("bassenc") || fileName.Contains("bassmix")) continue;
+            if (
+                fileName == "bass.dll" || 
+                fileName == "libbass.so" || 
+                fileName.Contains("bassenc") || 
+                fileName.Contains("bassfx") ||
+                fileName.Contains("bassmix")
+            ) continue;
 
             if (Bass.PluginLoad(file) != 0) continue;
             if (Bass.LastError == Errors.Already) continue;
